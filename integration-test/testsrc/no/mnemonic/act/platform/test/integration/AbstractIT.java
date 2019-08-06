ca411429edf21a510fbf08ded5b0f8249f1c6c91
Konstantin Mueller <konstantin@mnemonic.no>
2019-08-06 13:20:19 +0200

    ARGUS-13408: Upgraded ElasticSearch client and jackson-databind in order to resolve security warnings.

--- integration-test/testsrc/no/mnemonic/act/platform/test/integration/AbstractIT.java
+++ integration-test/testsrc/no/mnemonic/act/platform/test/integration/AbstractIT.java
@@ -65,7 +65,7 @@ public abstract class AbstractIT {
   private static ElasticSearchDockerResource elastic = ElasticSearchDockerResource.builder()
           // Need to specify the exact version here because Elastic doesn't publish images with the 'latest' tag.
           // Usually this should be the same version as the ElasticSearch client used.
-          .setImageName("elasticsearch/elasticsearch:6.8.1")
+          .setImageName("elasticsearch/elasticsearch:6.8.2")
           .setExposedPortsRange("15000-25000")
           .addApplicationPort(9200)
           .addEnvironmentVariable("xpack.security.enabled", "false")
